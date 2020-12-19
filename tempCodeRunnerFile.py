4b10c8a140182e44d81a8e3c5fc8cbbcc597cbd5
Serena-Guitar <serenaxy@qq.com>
2020-12-19 21:03:05 +0800

    delete

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    dict_key = {'ns', '华纺股份'}
-    graph = Graph('http://localhost:7474', username='neo4j', password='Serena')
-    cql = 'match (n:高管)-[:董事会成员]->(p:企业{name:\'华纺股份\'}) return n'
-    result = graph.run(cql)
-    print(result)
